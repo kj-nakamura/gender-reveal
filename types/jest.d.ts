2a0bc58fc295ea72f0ec66a9edd62821af0a5250
Kenji Nakamura <kenji.nkmr.1117@gmail.com>
2025-07-06 08:28:20 +0900

    ci

---
+++ types/jest.d.ts
@@ -0,0 +1,12 @@
+// types/jest.d.ts
+import '@testing-library/jest-dom'
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R
+      toHaveClass(className: string): R
+      toBeDisabled(): R
+    }
+  }
+}
