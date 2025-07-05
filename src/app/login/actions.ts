0acf97edd64724ae06b68cb3bac5e45077e1ce38
Kenji Nakamura <kenji.nkmr.1117@gmail.com>
2025-07-05 09:04:14 +0900

    移動

---
+++ src/app/login/actions.ts
@@ -0,0 +1,43 @@
+// app/login/actions.ts
+"use server";
+
+import { createClient } from "../../../lib/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const signup = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    // エラー処理（例: エラーページにリダイレクト）
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  // 新規登録後はメール確認を促すページにリダイレクトするのが親切
+  return redirect("/login?message=Check email to continue sign in process");
+};
+
+export const login = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  // ログイン成功後はメインページなどにリダイレクト
+  return redirect("/");
+};
